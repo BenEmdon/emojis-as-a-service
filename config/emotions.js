eeeb5356eb8f996bb768212e01d5aeed16cb0bed
Orajiaku Chukwudalu <chukwudalurjk@gmail.com>
2017-03-31 13:38:51 -0400

    Initial setup for getting images and a config file for emotions

---
+++ config/emotions.js
@@ -0,0 +1,11 @@
+/**
+ * Created by orajiakuchukwudalu on 17-03-31.
+ */
+module.exports = {
+    surprise    :   "SURPRISE",
+    sadness     :   "SADNESS",
+    anger       :   "ANGER",
+    disgust     :   "DISGUST",
+    fear        :   "FEAR",
+    joy         :   "JOY"
+};
---
+++ utils/actions.js
@@ -0,0 +1,12 @@
+/**
+ * Created by orajiakuchukwudalu on 17-03-31.
+ */
+module.exports = {
+    /**
+     *
+     * @param images This is an array of images. Each image is an object that will have the image and the coordinates of faces
+     */
+    getEmoji: function(images){
+
+    }
+};
