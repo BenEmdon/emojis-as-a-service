e49777f66f34a8904cafcd13b568da0f781f71a3
Orajiaku Chukwudalu <chukwudalurjk@gmail.com>
2017-03-31 13:53:36 -0400

    added emojis and link to get them

--- config/emotions.js
+++ config/emotions.js
@@ -1,11 +1,13 @@
 /**
  * Created by orajiakuchukwudalu on 17-03-31.
  */
+var emojibase = "../emojis";
+
 module.exports = {
-    surprise    :   "SURPRISE",
-    sadness     :   "SADNESS",
-    anger       :   "ANGER",
-    disgust     :   "DISGUST",
-    fear        :   "FEAR",
-    joy         :   "JOY"
+    surprise    :   {name: "SURPRISE", emoji: require(emojibase + "/surprise.png")},
+    sadness     :   {name: "SADNESS", emoji: require(emojibase + "/sadness.png")},
+    anger       :   {name: "ANGER", emoji: require(emojibase + "/anger.png")},
+    disgust     :   {name: "DISGUST", emoji: require(emojibase + "/disgust.png")},
+    fear        :   {name: "FEAR", emoji: require(emojibase + "/fear.png")},
+    joy         :   {name: "JOY", emoji: require(emojibase + "/joy.png")}
 };
