1a6faf3c000ed494a36db65395fcad4610a0dc33
Jackie Ellis <jackie.ellis@hotmail.com>
2017-03-31 13:31:03 -0400

    added public_html and localhost in app js

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
+var path = require('path');
+
+app.listen(2017,function(){
+	console.log('Server listening on port 2017');
+});
+
+app.post("/upload", (req, res) => {
+	
+	console.log("Image request");
+});
 
 app.get('/', (req, res) => {
-  res.send({'nice': 'meme'});
+
+	var options = {
+	    root: __dirname + '/public_html/',
+	    dotfiles: 'deny',
+	    headers: {
+	        'x-timestamp': Date.now(),
+	        'x-sent': true
+	    }
+	  };
+	//res.send({'nice': 'meme'});
+	res.sendFile("index.html", options, function(err) {
+		if (err) {
+	      console.log("Error");
+	    }
+	});
 });
