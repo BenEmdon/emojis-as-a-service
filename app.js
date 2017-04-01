b3cd448f88dc69644e79b24ca3630e226b31bda7
Fengwei Zhang <jerry.fengwei@gmail.com>
2017-04-01 12:35:10 -0400

    cleanup

--- app.js
+++ app.js
@@ -5,22 +5,19 @@ const api = require('./ApiRequest');
 const actions = require('./utils/actions');
 const bodyParser = require('body-parser');
 const imageURL = 'https://emojis-as-a-service.herokuapp.com/images/'
-
-dotenv.config({ silent: true });
-
+dotenv.config({
+    silent: true
+});
 var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
-
 const ROOT = "./public_html";
-
-app.use(function(req,res,next){
-  console.log(req.method + " request for " + req.url);
-  next();
+app.use(function (req, res, next) {
+    console.log(req.method + " request for " + req.url);
+    next();
 });
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded());
-
 // app.post("/upload", upload.single('pic'), function(req, res) {
 //  console.log(req.file);
 //  api(imageURL + req.file.filename)
@@ -33,76 +30,62 @@ app.use(bodyParser.json());
 //    res.send(error);
 //  });
 // });
-
-app.post('/upload', function(req, res){
-
-  // create an incoming form object
-  var form = new formidable.IncomingForm();
-
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
-
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/uploads');
-
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    fs.rename(file.path, file.path + path.extname(file.name), function(err) {
-      if (err) return res.send(err);
-      file.path = file.path + path.extname(file.name);
-      file.name = path.basename(file.path);
-      res.send(imageURL + file.name);
-      api(imageURL + file.name)
-        .then((imageData) => {
-          /*actions.getAllEmojis(imageData.frames[0], () => {
+app.post('/upload', function (req, res) {
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function (field, file) {
+        fs.rename(file.path, file.path + path.extname(file.name), function (err) {
+            if (err) return res.send(err);
+            file.path = file.path + path.extname(file.name);
+            file.name = path.basename(file.path);
+            res.send(imageURL + file.name);
+            api(imageURL + file.name).then((imageData) => {
+                /*actions.getAllEmojis(imageData.frames[0], () => {
         res.sendFile(file.path);
       })*/
-        })
-        .catch((error) => {
-          console.log(error);
-          res.send(error);
+            }).catch((error) => {
+                console.log(error);
+                res.send(error);
+            });
         });
     });
-  });
-
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    // res.end('success');
-  });
-
-  // parse the incoming request containing the form data
-  form.parse(req);
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        // res.end('success');
+    });
+    // parse the incoming request containing the form data
+    form.parse(req);
 });
-
-app.post('/slack', function (req, res){
-  console.log(req.body.image_url);
-    api(req.body.image_url)
-        .then((imageData) => {
-          /*actions.getAllEmojis(imageData.frames[0], () => {
+app.post('/slack', function (req, res) {
+    console.log(req.body.image_url);
+    api(req.body.image_url).then((imageData) => {
+        /*actions.getAllEmojis(imageData.frames[0], () => {
         res.sendFilexfile.path);
-      })*/        res.json({url:'http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg'});
-    })
-        .catch((error) => {
-          console.log(error);
-          res.send(error);
+      })*/
+        res.json({
+            url: 'http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg'
         });
+    }).catch((error) => {
+        console.log(error);
+        res.send(error);
+    });
 });
-
 app.use('/images', express.static('./uploads'));
-
-app.use(express.static(ROOT));  //handle all static requests
-
-app.all("*",function(req, res) {
-  res.sendStatus(404);
+app.use(express.static(ROOT)); //handle all static requests
+app.all("*", function (req, res) {
+    res.sendStatus(404);
 });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+    console.log(`Express server listening on port ${port}`);
 })
