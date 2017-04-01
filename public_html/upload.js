183b68018cf1b9f29282f3816594d0a358dd466b
Jackie Ellis <jackie.ellis@hotmail.com>
2017-04-01 16:45:50 -0400

    stringify object

--- public_html/upload.js
+++ public_html/upload.js
@@ -34,11 +34,11 @@ $(document).ready(function() {
         processData: false,
         contentType: false,
         success: function(data){
-            console.log('upload successful!\n' + data);
+            console.log('upload successful!\n' + JSON.stringify(data));
             $(".overlay").show();
             $(".uploadContent").hide();
             $("html").css("background-color", "#303030");
-            $("#outputtedImg").attr("src",data);
+            $("#outputtedImg").attr("src",JSON.stringify(data));
         },
         xhr: function() {
           // create an XMLHttpRequest
