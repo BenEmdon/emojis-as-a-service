b4787d9fb5747d516c5d12034b5e63503d9fc0e9
BenEmdon <benjaminemdon@gmail.com>
2017-04-01 16:03:28 -0400

    else if

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ function processAPIData(res, data, filename) {
     if (data.status_code === undefined) {
         console.log(data);
         res.send(data);
-    }
-    if (data.status_code === 4 && data.frames) {
+    } else if (data.status_code === 4 && data.frames) {
         console.log(`=> PRE-EMOJI: ${data.frames[0]}`);
         actions.getAllEmojis(data.frames[0], () => {
             console.log(`=> EMOJI: ${data.frames[0]}`);
